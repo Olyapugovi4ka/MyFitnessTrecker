1e421496d053f833622d4e479afb43f79afcb770
Olga Dakhel <omelnik@me.com>
2020-06-10 10:34:34 +0300

    Init

---
+++ MyFitnessTreckerTests/MyFitnessTreckerTests.swift
@@ -0,0 +1,34 @@
+//
+//  MyFitnessTreckerTests.swift
+//  MyFitnessTreckerTests
+//
+//  Created by ios_Dev on 10.06.2020.
+//  Copyright © 2020 ios_Dev. All rights reserved.
+//
+
+import XCTest
+@testable import MyFitnessTrecker
+
+class MyFitnessTreckerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
